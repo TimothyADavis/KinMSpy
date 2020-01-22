d4657dcbe2b87c34a36de21d6466f2baba10b809
James Dawson <SpaceMeerkat@github.com>
2020-01-22 16:01:05 +0000

    sys path changed for non local compiling

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('/home/corona/c1307135/KinMSpy'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
