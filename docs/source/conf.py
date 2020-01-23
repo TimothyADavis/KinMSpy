f45ad51be0a68abe175b21d915c9ef787680a19e
James Dawson <SpaceMeerkat@github.com>
2020-01-23 10:37:56 +0000

    logo added to documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,4 +55,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_theme_options = {
+        'logo_only':True
+        }
+html_logo = '../../logo_files/Logo.png'
